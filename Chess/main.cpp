3a0f1269346a49952e10297e6a55228497f6e5d1
David Sun <dsun05@g.ucla.edu>
2024-03-08 18:09:45 -0800

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by David Sun on 3/8/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
